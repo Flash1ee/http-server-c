67b53c27d9fa1404702d0a05cc4cd06598b8c9c5
Dmitry Varin <varindv@gmail.com>
2021-01-06 22:30:40 +0300

    add parse first string METHOD URL PROTOCOL

--- README.md
+++ README.md
@@ -11,13 +11,38 @@
    
 3. process
     daemonize
-            hserver (pid : hserver_pid, ppid: bash_pid, stdin, stdout, stderr)
-            close(stdin), close(stdout), close(stderr)
-            fork -> hserver_child(pid: hserver_child-pid, ppid: 1 (init_pid))
-            setsid();
-   setup_signals
-            SIGTERM -> exit
-   
-4. hserver
+            hserver (pid : hserver_pid, ppid: bash_pid, stdin, stdout, stderr)  
+            close(stdin), close(stdout), close(stderr)  
+            fork -> hserver_child(pid: hserver_child-pid, ppid: 1 (init_pid))  
+            setsid();  
+   setup_signals  
+            SIGTERM -> exit  
+     
+4. hserver  
+
+Обычный GET-запрос
+
+Запрос клиента:
+
+GET /wiki/страница HTTP/1.1
+Host: ru.wikipedia.org
+User-Agent: Mozilla/5.0 (X11; U; Linux i686; ru; rv:1.9b5) Gecko/2008050509 Firefox/3.0b5
+Accept: text/html
+Connection: close
+(пустая строка)
+
+Ответ сервера:
+
+HTTP/1.1 200 OK
+Date: Wed, 11 Feb 2009 11:20:59 GMT
+Server: Apache
+X-Powered-By: PHP/5.2.4-2ubuntu5wm1
+Last-Modified: Wed, 11 Feb 2009 11:20:59 GMT
+Content-Language: ru
+Content-Type: text/html; charset=utf-8
+Content-Length: 1234
+Connection: close
+(пустая строка)
+(запрошенная страница в HTML)
 
    
--- http.c
+++ http.c
@@ -1,10 +1,60 @@
+#include <string.h>
+#include <ctype.h>
 #include "http.h"
 
 #define BUFFER_SIZE 4096
 
+int readline(http_read_t read, void *data, char *buffer, size_t buffer_size) {
+    int i = 0;
+    while (i < BUFFER_SIZE) {
+        char c;
+        int r = read(data, &c, sizeof(c));
+        if (r <= 0) {
+            return -1;
+        }
+        if (c == '\n') {
+            break;
+        }
+        buffer[i++] = c;
+    }
+    if (i > 0 && buffer[i - 1] == '\r') {
+        i--;
+    }
+    buffer[i] = '\0';
+    return i;
+}
+
 int http_handler(http_read_t read, http_write_t write, void *data) {
     char buffer[BUFFER_SIZE];
-    int r = read(data, buffer, sizeof(buffer));
-    write(data, buffer, r);
+    int buf_len = readline(read, data, buffer, sizeof(buffer));
+    if (buf_len <= 0) {
+        return -1;
+    }
+    char *method_beg = buffer;
+    char *method_end = strchr(method_beg, ' ');
+    if (!method_end) {
+        return -1;
+    }
+
+    printf("method = %s\n", strndup(method_beg, method_end - method_beg));
+
+    char *url_beg = method_end + 1;
+    while (isspace(url_beg)) {
+        url_beg++;
+    }
+    char *url_end = strchr(url_beg, ' ');
+    if (!url_end) {
+        return -1;
+    }
+    printf("url = %s\n", strndup(url_beg, url_end - url_beg));
+
+    char *protocol_beg = url_end + 1;
+    while (isspace(url_beg)) {
+        url_beg++;
+    }
+    char *protocol_end = buffer + buffer_len;
+
+    printf("protocol = %s\n", strndup(protocol_beg, protocol_beg - protocol_end));
+
     return 0;
 }
